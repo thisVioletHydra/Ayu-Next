7e354ed36f365534129fbc556367bd28ab99ae60
VioletHydra <mrG0bliN.tm@gmail.com>
2020-06-21 17:56:18 +0300

    ⭐️ Autocommit [06/21/2020] - 17:56

---
+++ tmp/paint.js
@@ -0,0 +1,18 @@
+const obj = {
+  selection: '#33415e',
+  selection_border: '#323a4c',
+  selection_border: '#323a4c',
+  selection_border_width: '1',
+  selection_border_width: '1',
+  inactive_selection: '#323a4c',
+  inactive_selection: '#323a4c',
+};
+const obj2 = {
+  selection: '#34455a',
+  selection: '#33415e',
+  selection_border: '#323a4c',
+  selection_border: '#323a4c',
+  selection_border_width: '1',
+  selection_border_width: '1',
+  inactive_selection: '#323a4c',
+};
